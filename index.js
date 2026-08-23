7137c56abd6b86fcc7fc9a04d132cdf31c184e1b
Vamshi Reddy <vamsiprince6726574@gmail.com>
2026-08-23 23:38:06 +0530

    Shorten greeting message in root endpoint

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/health",(req,res)=>{
    return res.status(200).json({message:"all is good👍"})
 })
 app.get("/",(req,res)=>{
-   return res.status(200).json({message:"hello Vamshiiiiiiiiiiiiiiiiiiiiiiiiiiiiii👍"})
+   return res.status(200).json({message:"hello Vamshiii👍"})
 })
 
 app.listen(port,()=>{
